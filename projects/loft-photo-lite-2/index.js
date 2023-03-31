c8bc0937d1e1e864898186257954b8e4a2ea817e
hellkapwnz <104258955+hellkapwnz@users.noreply.github.com>
2023-03-31 09:39:49 +0300

    Delete projects directory

--- projects/loft-photo-lite-2/index.js
+++
@@ -1,9 +0,0 @@
-import pages from './pages';
-import('./styles.css');
-
-const pageNames = ['login', 'main', 'profile'];
-
-document.addEventListener('click', () => {
-  const pageNames = model.getRandomElement(pageNames);
-  pages.openPage(pageNames);
-});
